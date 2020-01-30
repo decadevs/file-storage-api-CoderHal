1e8ad4189655f2e3887379293dde5f39bc2d76d1
CoderHal <halof.halima@gmail.com>
2020-01-30 17:52:45 +0100

    create model for database

---
+++ filestore/buckets/models.py
@@ -0,0 +1,13 @@
+from djongo import models
+# from django.utils import timezone
+
+# Create your models here.
+class Bucket(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('name', max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+    def __str__(self):
+        return str(self.name)
