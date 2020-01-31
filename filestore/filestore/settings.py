58c8848a3a64a13106afbebd9884ebd4cbea382b
CoderHal <halof.halima@gmail.com>
2020-02-01 00:16:10 +0100

    create urls and serializer

--- filestore/filestore/settings.py
+++ filestore/filestore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+# from djangorestframework import rest_framework
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'bucket'
+    'rest_framework',
+    'buckets'
 ]
 
 MIDDLEWARE = [
