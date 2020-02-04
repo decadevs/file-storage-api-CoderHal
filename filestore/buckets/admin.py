99602ad0fa27fe33bfe0e6cd5f3a48c3f4d02e63
CoderHal <halof.halima@gmail.com>
2020-02-04 19:03:34 +0100

    continous integration

--- filestore/buckets/admin.py
+++ filestore/buckets/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import Bucket
+# from .models import Bucket
+from .models import Bucket
 
 admin.site.register(Bucket)
