b3103596c59dd2c9366fe9064ddbb50f9c74366c
CoderHal <halof.halima@gmail.com>
2020-02-06 09:05:57 +0100

    --- Auto-Git Commit ---

--- filestore/buckets/urls.py
+++ filestore/buckets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
-from .bucket import Bucket
+from .models import Bucket
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import BucketDetail, BucketList
 
--- filestore/buckets/views/bucket_detail.py
+++ filestore/buckets/views/bucket_detail.py
@@ -1,31 +1,10 @@
-# from .serializer import BucketSerializer
-from .bucket_serializer import BucketSerializer
 from rest_framework.views import APIView
-from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response 
 
-from .bucket import Bucket
-# from .models import Bucket
+from ..bucket import Bucket
+from ..bucket_serializer import BucketSerializer
 
-
-class BucketList(APIView):
-
-    def post(self, request, format=None):
-        ''' creates a bucket instance'''
-
-        serializer = BucketSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def get(self, request, format=None):
-
-        ''' lists all buckets '''
-        buckets = Bucket.objects.all()
-        serializer = BucketSerializer(buckets, many=True)
-        return Response(serializer.data)
 
 
 class BucketDetail(APIView):
