20e91dee5a47b5cba68d6f25a3f6d19001f85186
CoderHal <halof.halima@gmail.com>
2020-02-06 10:36:14 +0100

    --- Auto-Git Commit ---

--- filestore/buckets/tests/test_bucket_views.py
+++ filestore/buckets/tests/test_bucket_views.py
@@ -3,15 +3,15 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
-from .mock_data import bucket_data
-from ..models import Bucket
+from .mock_data.bucket_data import bucket_data
+from ..models.bucket import Bucket
 
 
 class TestBucketView(APITestCase):
     ''' Tests for different endpoints'''
 
     def setUp(self):
-        Bucket.objects.create(**bucket_data.bucket_data())
+        Bucket.objects.create(**bucket_data())
         self.url = reverse('bucket')+'?id=1' 
 
     def test_api_can_create_bucket(self):
